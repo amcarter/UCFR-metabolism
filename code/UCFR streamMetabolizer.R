b1d7c0ac21eb505c825dbe81688c07f509d933d9
Matt Trentman <mttrentman@gmail.com>
2021-12-03 14:51:14 -0700

    Update UCFR streamMetabolizer.R

--- code/UCFR streamMetabolizer.R
+++ code/UCFR streamMetabolizer.R
@@ -104,7 +104,7 @@ nb_DL <- mm_name(type='bayes', pool_K600='normal', err_obs_iid=TRUE, err_proc_ii
 DL_specs <- specs(nb_DL,K600_daily_meanlog_meanlog=1.986474396, K600_daily_meanlog_sdlog=0.75, 
                   K600_daily_sdlog_sigma=0.1, burnin_steps=500, saved_steps=500,verbose=T)
 
-DL_fit <- metab(Gar_specs, data=DL_dat)
+DL_fit <- metab(DL_specs, data=DL_dat)
 
 #Check model output
 DL_params<-get_params(DL_fit , uncertainty='ci')
@@ -120,6 +120,7 @@ names(dat)<-c("unix.time", "date.UTC", "date.MST", "battery", "temp.c", "do.mgl"
 #lubridate::as_datetime(dat$unix.time)
 dat$date<-as.Date(dat$date.UTC,format="%m-%d-%Y")
 
+dat<-subset(dat,dat$date != as.Date("2020-08-02") & dat$date != as.Date("2020-08-03"))
 
 # convert to solar time
 time<-as.POSIXct(paste(dat$date.UTC),format="%Y-%m-%d %H:%M:%S",tz="UTC")
@@ -130,28 +131,8 @@ dat$solar.time <- convert_UTC_to_solartime(
 )
 
 
-# Interpolate missing data
-starttime <- round_date(dat$solar.time[2],"15 minutes")
-endtime <- round_date(dat$solar.time[nrow(dat)-1],"15 minutes")
-number <- as.numeric(difftime(endtime,starttime,15,units = c("mins")))/15+1
-timeframe <- seq(starttime,endtime,length.out = number)
 
-
-#interpolate data to time frame
-do.inter <- approx(
-  x = dat$solar.time,
-  y = dat$do.mgl,
-  xout = timeframe
-);
-temp.water <- approx(
-  x = dat$solar.time,
-  y = dat$temp.c,
-  xout = timeframe
-);
-
-
-metab <- data.frame(timeframe,do.inter$y,temp.water$y)
-metab$date <- as.Date(as.POSIXct(metab$timeframe))
+metab<-dat[c(10,6,5,9)]
 
 colnames(metab) <- c("solar.time","DO.obs","temp.water", "date")
 
@@ -186,7 +167,7 @@ instFlow$q.m3s<-instFlow$X_00060_00003/35.31
 names(instFlow)<-c("agency", "site", "date","q.cfs","code", "tz", "q.cms")
 instFlow<-select(instFlow, c(-'agency', -site, -q.cfs, -code, -tz))
 
-metab<-merge(metab, instFlow, by="date", all.x=TRUE)
+metab<-left_join(metab, instFlow)
 
 # Depth- assign depth =1m so we can look at how different depths affect rates post modeling.
 metab$depth<-rep(1, times=length(metab$date))
@@ -197,21 +178,22 @@ GAR_dat<-tibble(solar.time=metab$solar.time,DO.obs=metab$DO.obs,DO.sat=metab$DO.
 #Run model
 nb_GAR<- mm_name(type='bayes', pool_K600='normal', err_obs_iid=TRUE, err_proc_iid=TRUE)
 
-GAR_specs <- specs(nb_GAR, burnin_steps=500, saved_steps=500,verbose=T)
+GAR_specs <- specs(nb_GAR, K600_daily_meanlog_meanlog=1.986474396, K600_daily_meanlog_sdlog=0.75, K600_daily_sdlog_sigma=0.1, burnin_steps=500, saved_steps=500,verbose=T)
 
-GAR_fit <- metab(GAR_specs, data=GAR_dat)
+GAR_fit <- metab(specs=GAR_specs, data=GAR_dat)
 
 GAR_params<-get_params(GAR_fit , uncertainty='ci')
 GAR_mcmc<-get_mcmc(GAR_fit)
-print(GAR_fit)
+launch_shinystan(GAR_mcmc)
 
 
+GAR_fit
 
+plot_DO_preds(GAR_fit)
+get_fit(GAR_fit)$overall %>%
+  select(ends_with('Rhat'))
 
-
-
-
-
+rstan::traceplot(GAR_mcmc, pars='err_proc_iid_sigma', nrow=3)
 
 
 
