115f432b7d9b1620242cadbca54a5951403cb5a8
Alice Carter <alicecarter05@gmail.com>
2022-11-15 16:37:09 -0700

    added a grouping variable to biomass stan model

--- code/model/GPP_biomass_model.stan
+++ code/model/GPP_biomass_model.stan
@@ -1,28 +1,39 @@
-// STAN code to run linear models of GPP using modeled Biomass data
-
-
 data {
-  int<lower=0> N;
-  vector[N] P;       // productivity (response)
-  // vector[N] site;    // grouping variable
-  vector[N] light;   // covariate
-  vector[N] biomass; // covariate
-  // vector[N] epil_mean;
+  int<lower=0> N;             // total number of observations
+  int<lower=1> K;             // number of covariates + intercept
+  int<lower=1> S;             // number of groups (sites)
+  int<lower=1,upper=S> ss[N]; // site for each observation
+  matrix[N,K] X;              // covariate matrix
+  vector[N] P;                // productivity (response)
 }
 
 parameters {
-  vector[3] beta;       // intercept and covariate parameters
-  real<lower=0> sigma;  // sd of innovations
+  vector[K] gamma;       // population level coefficientss
+  vector<lower=0>[K] tau;//standard deviation of regression coefficients
+
+  vector[K] beta[S];     //matrix of site level reg coefficients
+  real<lower=0> sigma;   // sd of individual observations
 }
 
 model {
 
+  vector[N] mu; //linear predictor
+
   //priors
-  beta ~ normal(0,5);
-  sigma~ cauchy(0,1);
+  gamma ~ normal(0,5);
+  tau ~ cauchy(0,2.5);
+  sigma ~ gamma(2,1);
+
+  for(s in 1:S){
+      beta[s] ~ normal(gamma,tau);
+  }
+
+  for(n in 1:N){
+      mu[n] = X[n]*beta[ss[n]];
+  }
 
   //likelihood
-  P ~ normal(beta[1] + beta[2] * light + beta[3] * biomass, sigma);
+  P ~ normal(mu, sigma);
 
 }
 
--- code/model/test_GPP_biomass_stan_model.R
+++ code/model/test_GPP_biomass_stan_model.R
@@ -11,25 +11,43 @@ library(rstan)
 dat <- read_csv('data/biomass_metab_model_data.csv')
 
 # set params
-n <- 100
-b0 <- 1
-b1 <- 3
-b2 <- 1
+n <- 600
+gamma <- c(1,3,1)       # population level coefficients
+tau <- 1                # standard deviation of regression coefficients
+sites <- seq(1:6)       # unique sites
+K <- length(gamma)      # number of parameters
+S <- length(sites)      # number of sites
+ss <- rep(sites, n/S)   # site of each observation
+
 light <- abs(rnorm(n = n))
 light <- light/max(light)
-
 biomass <- abs(rnorm(n = n))
+
+X <- matrix(c(rep(1, n), light, biomass), ncol = 3) # model matrix
 sigma_epsilon <- 0.5
 
-# simulate response vector
-P <- vector(mode = "double", length = n)
-P <- rnorm(n, mean = b0 + b1 * light + b2 * biomass, sd = sigma_epsilon)
+beta <- matrix(NA, ncol = K, nrow = S)
+for(i in 1:S){
+    beta[i,] <- rnorm(3, gamma, tau)
+}
 
+# simulate response vector
+mu <- vector(mode = 'double', length = n)
+
+for(i in 1:n){
+    mu[i] <- X[i,]%*%beta[ss[i],]
+}
+
+P <- vector(mode = "double", length = n)
+P <- rnorm(n, mean = mu, sd = sigma_epsilon)
+
+sim <- data.frame(site = ss, light = light, biomass = biomass, GPP = P)
+ggplot(sim, aes(biomass, GPP, col = factor(site))) +
+    geom_point()
 # compile data for stan
 datlist <- list(
-    N = n,
-    light = light,
-    biomass = biomass,
+    N = n, K = K, S = S,
+    ss = ss, X = X,
     P = P
 )
 
@@ -43,15 +61,24 @@ mfit <- sampling(
 )
 
 
-plot(mfit)
-traceplot(mfit)
+plot(mfit, pars = c('beta[1,1]','beta[2,1]','beta[3,1]',
+                    'beta[4,1]','beta[5,1]','beta[6,1]'))
+plot(mfit, pars = c('beta[1,2]','beta[2,2]','beta[3,2]',
+                    'beta[4,2]','beta[5,2]','beta[6,2]'))
+plot(mfit, pars = c('beta[1,3]','beta[2,3]','beta[3,3]',
+                    'beta[4,3]','beta[5,3]','beta[6,3]'))
+traceplot(mfit, pars = 'beta')
 
 # test on dataset:
-dat <- dplyr::filter(dat, !is.na(GPP), !is.na(light), !is.na(epil_chla_mgm2_fit))
+dat <- dplyr::filter(dat, !is.na(GPP), !is.na(light), !is.na(fila_chla_mgm2_fit))
+
+X_dat <- matrix(c(rep(1, nrow(dat)), dat$light, dat$fila_chla_mgm2_fit), ncol = 3)
+ss_dat <- as.numeric(factor(dat$site, levels = c('PL', 'DL', 'GR', 'GC', 'BM', 'BN')))
+
 datlist <- list(
     N = nrow(dat),
-    light = dat$light,
-    biomass = dat$epil_chla_mgm2_fit,
+    K = 3, S = 6,
+    ss = ss_dat, X = X_dat,
     P = dat$GPP
 )
 
@@ -60,5 +87,5 @@ dfit <- sampling(
     data = datlist
 )
 
-plot(dfit)
+plot(dfit, pars = c('beta', 'sigma'))
 traceplot(dfit)
