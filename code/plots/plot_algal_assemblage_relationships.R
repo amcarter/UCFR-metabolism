384a62834ad58c79950cb8178d64e7d92ff8ceea
Alice Carter <alicecarter05@gmail.com>
2023-11-04 12:09:59 -0600

    made algal assemblage plots and modified turnover time plot

--- code/plots/plot_algal_assemblage_relationships.R
+++ code/plots/plot_algal_assemblage_relationships.R
@@ -2,6 +2,7 @@
 # plot the role of algal assemblage in fluxes and turnovers
 
 library(tidyverse)
+library(lme4)
 
 
 met <- read_csv('data/metabolism/metabolism_compiled_all_sites_mle_fixedK.csv') %>%
@@ -12,7 +13,7 @@ q90 <- read_csv('data/quantile_PR_fits_summary.csv')
 met <- left_join(met, select(q90, site, year, ARf), by = c('site', 'year')) %>%
     mutate(year = factor(year),
            NPP = GPP * (1-ARf),
-           NPP_globalARf = GPP * (1 + beta[2,1]),
+           # NPP_globalARf = GPP * (1 + beta[2,1]),
            AR = -GPP*(ARf),
            HR = ER - AR) %>%
     select(-msgs.fit, -warnings, -errors, -K600, -DO_fit)
@@ -26,7 +27,7 @@ min_fila_gm2 <- filter(bio, filamentous_gm2>0) %>% select(filamentous_gm2) %>% m
 # Calculate average site year metrics:
 NPP <- met %>%
     group_by(site, year) %>%
-    summarize(across(any_of(c('GPP', 'ER', 'ARf', 'NPP', 'NPP_globalARf',
+    summarize(across(any_of(c('GPP', 'ER', 'ARf', 'NPP',
                               'AR', 'HR')),
                      mean, na.rm = T))
 
@@ -74,7 +75,7 @@ npp <- NPP %>%
            GPP_C = GPP * 12/32,
            ER_C = ER * 12/32,
            NPP_C = NPP * 12/32,
-           NPP_C_globalARf = NPP_globalARf * 12/32,
+           # NPP_C_globalARf = NPP_globalARf * 12/32,
            days_bio = biomass_C/NPP_C) %>%
     ungroup() %>%
     mutate(fila_bloom = c(0,1,0,1,0,0,1,1,1,0,0,0),
@@ -91,7 +92,7 @@ plot(density(npp$days_bio))
 ggplot(npp, aes(frac_fila_gm, GPP))+
     geom_point(aes(col = fila_bloom), size = 1.5)
 npp %>%
-    pivot_longer(cols = c('GPP', 'ER', 'ARf', 'NPP_C', 'NPP_C_globalARf',
+    pivot_longer(cols = c('GPP', 'ER', 'ARf', 'NPP_C',
                           'biomass_C', 'biomass_chla', 'days_bio'),
                  names_to = 'response',
                  values_to = 'value') %>%
@@ -112,36 +113,52 @@ cor.test(npp$frac_fila_gm, npp$biomass_C, method = 'pearson')
 cor.test(npp$frac_fila_gm, npp$days_bio, method = 'pearson')
 
 corrplot::corrplot(ff_cor)
+labs <- data.frame(response = c('A_GPP_C', 'B_ER_C', 'C_ARf', 'D_NPP_C',
+                     'E_biomass_C', 'F_days_bio'),
+                   corr = c('', '', '', '',
+                            paste0('r = ', round(ff_cor[1,6], 2), '0'),
+                            paste0('r = ', round(ff_cor[1,7], 2))))
 
 
 png('figures/algal_assemblage_relationships.png',
-    width = 5, height = 6, units = 'in', res = 300)
+    width = 4.5, height = 5.4, units = 'in', res = 300)
     npp %>%
-        select(site, year, fila_bloom, frac_fila_gm, A_GPP_C = GPP_C, B_ER_C = ER_C,
+        select(site, year, fila_bloom, fila_gm, frac_fila_gm, A_GPP_C = GPP_C, B_ER_C = ER_C,
                C_ARf = ARf, D_NPP_C = NPP_C, E_biomass_C = biomass_C,
                F_days_bio = days_bio) %>%
+        mutate(C_ARf = 100*C_ARf,
+               fila_bloom = factor(fila_bloom, levels = c('No Bloom', 'Bloom'))) %>%
         pivot_longer(cols = any_of(c('A_GPP_C', 'B_ER_C', 'C_ARf', 'D_NPP_C',
                                      'E_biomass_C', 'F_days_bio')),
                      names_to = 'response',
                      values_to = 'value') %>%
         ggplot(aes(frac_fila_gm, value)) +
-        # ggplot(aes(frac_fila_gm, value, pch = factor(fila_bloom))) +
-        geom_point(size = 1.6) +
+        geom_point(aes(col = fila_gm), size = 1.6) +
+        scale_color_continuous(expression('Filamentous Algae (g m'^{-2}*')'))+
+        # geom_point(aes(shape = fila_bloom,
+        #            col = fila_bloom), size = 1.6) +
+        # scale_color_manual("", values = c('grey75', '#97BB43'))+
+        # scale_shape_manual("", values = c(19,17))+
         facet_wrap(response~., scales = 'free_y',
                    strip.position = 'left',
                    nrow = 3,
-                   labeller = as_labeller(c(A_GPP_C = 'GPP (gC/m2/d)', B_ER_C = 'ER (gC/m2/d)',
-                                            C_ARf = 'Autotrophic Respiration Fraction',
-                                            D_NPP_C = 'NPP (gC/m2/d)',
-                                            E_biomass_C = 'Biomass (gC/m2)',
-                                            F_days_bio = 'Turnover Time (d)'))) +
-        labs(y = NULL, x = 'Filamentous fraction of total biomass (%)') +
+                   labeller = as_labeller(c(A_GPP_C = 'GPP~(g~C~m^{-2}~d^{-1})',
+                                            B_ER_C = 'ER~(g~C~m^{-2}~d^{-1})',
+                                            C_ARf = 'AR~Fraction~("%")',
+                                            D_NPP_C = 'NPP~(g~C~m^{-2}~d^{-1})',
+                                            E_biomass_C = 'Biomass~(g~C~m^{-2})',
+                                            F_days_bio = 'Turnover~Time~(d)'),
+                                          default = label_parsed)) +
+        labs(y = NULL, x = 'Filamentous fraction of total biomass (%)')+
+        annotate("segment", x=-Inf, xend=Inf, y=-Inf, yend=-Inf)+
+        geom_text(data = labs, aes(label = corr, x = -Inf, y = Inf),
+                  size = 3, hjust = -0.5, vjust = 3)+
         theme_classic()+
         theme(strip.background = element_blank(),
               strip.placement = "outside",
-              panel.border = element_rect(fill = NA))
+              legend.title = element_text(size = 9, vjust = 0.8),
               # legend.title = element_blank(),
-              # legend.position = 'top')
+              legend.position = 'top')
 
 dev.off()
 
@@ -186,10 +203,10 @@ ggplot(bm_met, aes(NPP, 0.435 * epil_gm2*light + 0.02394*fila_gm2*light,
 coefs <- fixef(modmix)
 
 bm_met <- bm_met %>%
-    mutate(fila_prod_gCd = (coefs[2] * fila_chla_mgm2*light) * 14/32 ,
+    mutate(fila_prod_gCd = (coefs[2] * fila_chla_mgm2*light) * 12/32 ,
            fila_turnover = case_when(fila_gm2 > min_fila_gm2 ~ fila_gm2/2/fila_prod_gCd,
                                      TRUE ~ NA_real_),
-           epil_prod_gCd = (coefs[1] * epil_chla_mgm2*light) * 14/32 ,
+           epil_prod_gCd = (coefs[1] * epil_chla_mgm2*light) * 12/32 ,
            epil_turnover = epil_gm2/2/epil_prod_gCd)
 bm_met %>%
     filter(year == 2021) %>%
@@ -231,17 +248,9 @@ p2 <- ggplot(npp, aes(fila_gm/2/(biomass_C)*100, days_bio))+
     theme(legend.title = element_blank(),
           legend.position = c(0.22, 0.85),
           panel.border = element_rect(fill = NA))
-p2B <- ggplot(npp, aes(fila_gm/2/(biomass_C)*100, NPP_C))+
-    xlab('Filamentous fraction of \ntotal biomass (%)')+
-    ylab('Mean daily production (gC/m2/d)')+
-    geom_point(aes(pch = factor(fila_bloom)), size = 1.6, col = 'black')+
-    theme_classic()+
-    theme(legend.title = element_blank(),
-          legend.position = 'none',
-          panel.border = element_rect(fill = NA))
 png('figures/turnover_time_by_composition.png',
     width = 6.5, height = 3.5, units = 'in', res = 300)
-ggpubr::ggarrange(p2B, p2, common.legend = TRUE)
+ggpubr::ggarrange(p1, p2, common.legend = TRUE)
 dev.off()
 
 ggplot(bm_met, aes(date, fila_prod_gCd))+
@@ -383,10 +392,10 @@ p4 <- bm_sum %>%
           legend.spacing.y = unit(-0.1, "cm"),
           legend.position = c(0.24, 0.79),
           legend.background = element_rect(fill = NA))
-png('figures/biomass_cumulative_and_turnover.png', width = 9, height = 3.5,
+png('figures/biomass_cumulative_and_turnover.png', width = 6.5, height = 3.5,
     units = 'in', res = 300)
-ggpubr::ggarrange(p4, p1,p2, ncol = 3,
-                  labels = c('A', 'B', 'C'),
+ggpubr::ggarrange(p4, p1, ncol = 2,
+                  labels = c('A', 'B'),
                   align = 'h')
 
 dev.off()
@@ -515,26 +524,45 @@ bm_met %>%
     geom_point() +
     geom_point(aes(y = epil_mgchlagC), col = 2) +
     facet_grid(site~year, scales = 'free_x')
-# Comparison to individual quantile regressions: ####
 
-library(quantreg)
-q90 <- data.frame(siteyear = unique(met$siteyear),
-                  slope = NA_real_,
-                  yintercept = NA_real_)
+dd <- bm_met %>%
+    select(site, year, date, epil_gm2, fila_gm2, GPP, ER, NPP, ARf,
+           fila_prod_gCd, epil_prod_gCd) %>%
+    mutate(GPP = GPP * 12/32,
+           ER = ER *12/32,
+           NPP = NPP *12/32,
+           AR = GPP*ARf,
+           HR = ER+AR,
+           fila_gC = fila_gm2/2,
+           epil_gC = epil_gm2/2)
 
-for(s in unique(met$siteyear)){
-    d <- filter(met, siteyear == s)
-    rqfit <- rq(ER ~ GPP, tau = 0.9, data = d)
-    q90$slope[q90$siteyear == s] <- summary(rqfit)$coefficients[2,1]
-    q90$yintercept[q90$siteyear == s] <- summary(rqfit)$coefficients[1,1]
-}
+dd$fila_diff = c(0, diff(dd$fila_gC) - dd$fila_prod_gCd[1:(nrow(dd)-1)])
+dd$epil_diff = c(0, diff(dd$epil_gC) - dd$epil_prod_gCd[1:(nrow(dd)-1)])
 
-q90 <- q90 %>%
-    mutate(site = substr(siteyear, 1,2),
-           year = as.numeric(substr(siteyear, 4,7)))
+ids <- dd %>%
+    mutate(r_number = row_number()) %>%
+    group_by(site, year) %>%
+    summarise(index = min(r_number))
+dd$epil_diff[ids$index] <- dd$fila_diff[ids$index] <- NA
 
-ggplot(met, aes(GPP, ER)) +
-    geom_point() +
-    geom_abline(data = q90, aes(intercept = yintercept,
-                                slope = slope))
-facet_grid(site~year)
+dd %>%
+    mutate(algal_loss = fila_diff + epil_diff - HR) %>%
+    pivot_longer(cols = ends_with('diff'),
+                 values_to = 'loss', names_to = 'algae') %>%
+    mutate(loss = case_when(loss>0 ~ 0,
+                     TRUE ~ loss))%>%
+    ggplot(aes(date, loss, fill = algae))+
+    geom_area()+
+    geom_line(aes(y = HR))+
+    facet_grid(site~year, scales = 'free_x') +
+    theme_classic()
+
+ggplot(data, aes(x=time, y=value, fill=group)) +
+    geom_area()
+
+filter(dd, site == 'DL', year == 2020)
+fila_prod_gCd/(fila_prod_gCd + epil_prod_gCd),
+           epil_AR = AR * epil_prod_gCd/(fila_prod_gCd + epil_prod_gCd))
+
+
+
